8aa945af31e51a96b65cae47bac12a911b5f8a89
sunit_pal <sunit.pal@dellteam.com>
2025-12-04 11:43:35 +0530

    products

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -46,47 +46,47 @@ export class ProductList {
   ngOnInit() {
     this.products = [
       {
+        name: '4 Side Seal Multitrack Pouch Packaging Machine',
+        description: 'Multitrack pouch packing machine. used for any kind of liquid for about 1 ml to 100ml in single pouch. up to 800 pouches per minute',
+        price: 22000,
+        image: 'assets/p16.png',
+        category: 'Machinery'
+      },
+      {
+        name: 'Center,Four Side Sealing Automatic Multi Track Pouch Packing Machin',
+        description: 'We are engaged in offering a wide range of Multi Track Pouch Packing Machine with the above specifications.',
+        price: 22000,
+        image: 'assets/p17.png',
+        category: 'Machinery'
+      },
+      {
+        name: 'Automatic aerosol paint can bucket labeling machine for all round can or bucket',
+        description: 'Automatic Aerosol Paint Can Bucket Labeling Machine For All Round Can Or Bucket , Find Complete Details about Automatic Aerosol Paint Can Bucket Labeling Machine For All Round Can Or Bucket,Paint Can .',
+        price: 22000,
+        image: 'assets/p18.png',
+        category: 'Machinery'
+      },
+      {
+        name: 'Automatic Double Side Flat Bottle Sticker Labeling Machine',
+        description: 'Automatic Double Side Flat Bottle Sticker Labelling Machine Model LI-BSL 2S is one of the most user friendly. Suitable for applying Front, Back or Side Labels on Flat/Oval/Square/Round shape containers with output Up to 100 Labels per minute depending on products Shape/Size and label size.',
+        price: 22000,
+        image: 'assets/p19.png',
+        category: 'Machinery'
+      },
+      {
+        name: 'Automatic Labeling Machine',
+        description: 'Cosmetics, Drinks, Cleaning, Detergent, Skin Care Products, Hair Care Products, Oil, Tea, Vegetable, Fruit, Fish, Meat, Snack, Rice, Flour, Seasoning, Dairy Products',
+        price: 22000,
+        image: 'assets/p20.png',
+        category: 'Machinery'
+      },
+      {
         name: 'Horizontal Automatic Sealing Machine',
         description: 'Sealing machines are used to seal containers with liquids, granules, powders, and sprays for consumer, bulk, and original equipment manufacturer (OEM) supply shipments. Packaging materials include aerosol containers, bags and pouches, blister packs, bottles and jars, as well as cartons and boxes.',
         price: 0,
         image: 'assets/p1.png',
         category: 'Machinery'
-      },
-      {
-        name: 'Auger Base Form Fill And Seal Machine',
-        description: 'Auger fillers are machines which fill the right amount of a product into its container. The product is stored in a vessel called the hopper and in each cycle, the filler dispenses a predetermined amount of the product into the package.',
-        price: 12000,
-        image: 'assets/p2.png',
-        category: 'Metal'
-      },
-      {
-        name: 'Automatic FFS Machine For Liquid',
-        description: 'The Automatic Liquid Packaging Machine is designed to pack various kinds of liquids as water, juices, vegetable oil, milk, soup, beverage, vinegar, etc. Automatic liquid packing machine is capable of making bags, measuring, filling, sealing, cutting and counting. The whole process operates automatically.',
-        price: 85000,
-        image: 'assets/p3.png',
-        category: 'Machinery'
-      },
-      {
-        name: 'Commercial Baking Oven, for Bakery',
-        description: 'The Combination oven provides room for your ambitions. It combines the active heat of the convection baking oven with the stationary baking atmosphere of the deck baking oven, thus combining two oven systems in minimum space (which can also be used as a suitable proofing cabinet) for exceptional baking variety and very high sophistication.',
-        price: 18000,
-        image: 'assets/p4.png',
-        category: 'Electrical'
-      },
-      {
-        name: 'Automatic Sealing Machine',
-        description: 'Plastic sealing machines also find uses in the packaging industry as they are much more affordable for packing than other materials. Hardware products like nuts and screws can be packed with automatic sealing machines.',
-        price: 22000,
-        image: 'assets/p5.png',
-        category: 'Construction'
-      },
-      {
-        name: 'Candy Packing Machine',
-        description: 'Packaging machines are used to package products or components. This product area includes equipment that forms, fills, seals, wraps, cleans, and packages at different levels of automation. Packaging machines also include related machinery for sorting, counting, and accumulating. There are many different package types.',
-        price: 22000,
-        image: 'assets/p6.png',
-        category: 'Machinery'
-      },
+      },      
       {
         name: 'Horizontal Pillow Pack Machine',
         description: "The M-D Servo type horizontal pillow pack is a compact and powerful flow wrapper that is suitable for a variety of applications. The machine user conveyor belt infeed's with a box motion cross sealer to ensure consistent and secure end seal and cutting.",
@@ -149,40 +149,40 @@ export class ProductList {
         price: 22000,
         image: 'assets/p15.png',
         category: 'Construction'
+      },      
+      {
+        name: 'Auger Base Form Fill And Seal Machine',
+        description: 'Auger fillers are machines which fill the right amount of a product into its container. The product is stored in a vessel called the hopper and in each cycle, the filler dispenses a predetermined amount of the product into the package.',
+        price: 12000,
+        image: 'assets/p2.png',
+        category: 'Metal'
       },
       {
-        name: '4 Side Seal Multitrack Pouch Packaging Machine',
-        description: 'Multitrack pouch packing machine. used for any kind of liquid for about 1 ml to 100ml in single pouch. up to 800 pouches per minute',
-        price: 22000,
-        image: 'assets/p16.png',
+        name: 'Automatic FFS Machine For Liquid',
+        description: 'The Automatic Liquid Packaging Machine is designed to pack various kinds of liquids as water, juices, vegetable oil, milk, soup, beverage, vinegar, etc. Automatic liquid packing machine is capable of making bags, measuring, filling, sealing, cutting and counting. The whole process operates automatically.',
+        price: 85000,
+        image: 'assets/p3.png',
         category: 'Machinery'
       },
       {
-        name: 'Center,Four Side Sealing Automatic Multi Track Pouch Packing Machin',
-        description: 'We are engaged in offering a wide range of Multi Track Pouch Packing Machine with the above specifications.',
-        price: 22000,
-        image: 'assets/p17.png',
-        category: 'Machinery'
+        name: 'Commercial Baking Oven, for Bakery',
+        description: 'The Combination oven provides room for your ambitions. It combines the active heat of the convection baking oven with the stationary baking atmosphere of the deck baking oven, thus combining two oven systems in minimum space (which can also be used as a suitable proofing cabinet) for exceptional baking variety and very high sophistication.',
+        price: 18000,
+        image: 'assets/p4.png',
+        category: 'Electrical'
       },
       {
-        name: 'Automatic aerosol paint can bucket labeling machine for all round can or bucket',
-        description: 'Automatic Aerosol Paint Can Bucket Labeling Machine For All Round Can Or Bucket , Find Complete Details about Automatic Aerosol Paint Can Bucket Labeling Machine For All Round Can Or Bucket,Paint Can .',
+        name: 'Automatic Sealing Machine',
+        description: 'Plastic sealing machines also find uses in the packaging industry as they are much more affordable for packing than other materials. Hardware products like nuts and screws can be packed with automatic sealing machines.',
         price: 22000,
-        image: 'assets/p18.png',
-        category: 'Machinery'
+        image: 'assets/p5.png',
+        category: 'Construction'
       },
       {
-        name: 'Automatic Double Side Flat Bottle Sticker Labeling Machine',
-        description: 'Automatic Double Side Flat Bottle Sticker Labelling Machine Model LI-BSL 2S is one of the most user friendly. Suitable for applying Front, Back or Side Labels on Flat/Oval/Square/Round shape containers with output Up to 100 Labels per minute depending on products Shape/Size and label size.',
+        name: 'Candy Packing Machine',
+        description: 'Packaging machines are used to package products or components. This product area includes equipment that forms, fills, seals, wraps, cleans, and packages at different levels of automation. Packaging machines also include related machinery for sorting, counting, and accumulating. There are many different package types.',
         price: 22000,
-        image: 'assets/p19.png',
-        category: 'Machinery'
-      },
-      {
-        name: 'Automatic Labeling Machine',
-        description: 'Cosmetics, Drinks, Cleaning, Detergent, Skin Care Products, Hair Care Products, Oil, Tea, Vegetable, Fruit, Fish, Meat, Snack, Rice, Flour, Seasoning, Dairy Products',
-        price: 22000,
-        image: 'assets/p20.png',
+        image: 'assets/p6.png',
         category: 'Machinery'
       }
     ];
