d4a4b91461f030ee92e77ae95d82a9e9757a18c2
sunit_pal <sunit.pal@dellteam.com>
2025-12-05 14:17:42 +0530

    email

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
-
-  onSubmit() {
-    alert('Message sent successfully!');
+  name: any;
+  email: any;
+  message: any;
+  reset(){
+    this.name="",
+    this.email=""
+    this.message=""
   }
+  send() {    
+    const params = {    
+    from_name: this.name,
+    from_email: this.email,
+    message: this.message,
+  };
+
+  emailjs.send(
+    'service_2aqbxgk',
+    'template_42j9ioe',
+    params,
+    '9EYLUT9xGYDE8bzxB'
+  )
+  .then(() => alert("Email Sent!"))
+  .catch(err => console.error(err));
+  this.reset();
+}
+
 }
